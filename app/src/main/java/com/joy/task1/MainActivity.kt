cca4056fba679eb7f2d7cc783fd6754cf4afcfd5
joyasturay <joyasturay1@gmail.com>
2024-06-10 17:57:19 +0530

    Hello World

---
+++ app/src/main/java/com/joy/task1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.joy.task1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    }
